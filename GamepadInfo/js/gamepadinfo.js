2598256c123127e20215635eac39608874692549
quizzlesticks <quizzlesticks@gmail.com>
2021-01-08 07:20:19 -0700

    Made the colors for the ps5 controller prettier.

--- GamepadInfo/js/gamepadinfo.js
+++ GamepadInfo/js/gamepadinfo.js
@@ -17,15 +17,15 @@ let display_looper;
 let controller;
 
 const ControllerDrawSettings = {
-	outline_color: "#ff0000"
-	//outline_color: "#bbb7b7e6"
+	//outline_color: "#ff000f"
+	outline_color: "#bbb7b7e6"
 };
 
 const drawController = function() {
 	ctx.fillStyle = "#201A23";
 	ctx.fillRect(0,0,canvas.width,canvas.height);
 	//LEFT WHITE PANEL
-	ctx.strokeStyle = "#FF0000";
+	ctx.strokeStyle = ControllerDrawSettings.outline_color;
 	ctx.beginPath();
 	ctx.moveTo(136,125);
 	ctx.bezierCurveTo(28,330,38,542,63,615);
@@ -37,7 +37,7 @@ const drawController = function() {
 	ctx.bezierCurveTo(213,105,195,104,136,125);
 	ctx.stroke();
 	//RIGHT WHITE PANEL
-	ctx.strokeStyle = "#FF0000";
+	ctx.strokeStyle = ControllerDrawSettings.outline_color;
 	ctx.beginPath();
 	ctx.moveTo(1000-136,125);
 	ctx.bezierCurveTo(1000-28,330,1000-38,542,1000-63,615);
@@ -49,7 +49,7 @@ const drawController = function() {
 	ctx.bezierCurveTo(1000-213,105,1000-195,104,1000-136,125);
 	ctx.stroke();
 	//BLACK LINE BOTTOM LEFT and WHITECURVE INSIDE HANDLE
-	ctx.strokeStyle = "#FF0000";
+	ctx.strokeStyle = ControllerDrawSettings.outline_color;
 	ctx.beginPath();
 	ctx.moveTo(122,675);
 	ctx.bezierCurveTo(172,680,174,680,175,681);
@@ -58,7 +58,7 @@ const drawController = function() {
 	ctx.bezierCurveTo(224,637,225,491,290,476);
 	ctx.stroke();
 	//BLACK LINE BOTTOM RIGHT and WHITECURVE INSIDE HANDLE
-	ctx.strokeStyle = "#FF0000";
+	ctx.strokeStyle = ControllerDrawSettings.outline_color;
 	ctx.beginPath();
 	ctx.moveTo(1000-122,675);
 	ctx.bezierCurveTo(1000-172,680,1000-174,680,1000-175,681);
@@ -67,21 +67,21 @@ const drawController = function() {
 	ctx.bezierCurveTo(1000-224,637,1000-225,491,1000-290,476);
 	ctx.stroke();
 	//UNDER THUMBPAD LEFT
-	ctx.strokeStyle = "#FF0000";
+	ctx.strokeStyle = ControllerDrawSettings.outline_color;
 	ctx.beginPath();
 	ctx.moveTo(306,472);
 	ctx.bezierCurveTo(330,471,327,483,385,476);
 	ctx.bezierCurveTo(409,476,452,477,500,477);
 	ctx.stroke();
 	//UNDER THUMBPAD RIGHT
-	ctx.strokeStyle = "#FF0000";
+	ctx.strokeStyle = ControllerDrawSettings.outline_color;
 	ctx.beginPath();
 	ctx.moveTo(1000-306,472);
 	ctx.bezierCurveTo(1000-330,471,1000-327,483,1000-385,476);
 	ctx.bezierCurveTo(1000-409,476,1000-452,477,1000-500,477);
 	ctx.stroke();
 	//LED LEFT
-	ctx.strokeStyle = "#FF0000";
+	ctx.strokeStyle = ControllerDrawSettings.outline_color;
 	ctx.beginPath();
 	ctx.moveTo(313,111);
 	ctx.bezierCurveTo(296,121,313,150,321,197);
@@ -89,7 +89,7 @@ const drawController = function() {
 	ctx.bezierCurveTo(458,297,486,297,500,297);
 	ctx.stroke();
 	//LED RIGHT
-	ctx.strokeStyle = "#FF0000";
+	ctx.strokeStyle = ControllerDrawSettings.outline_color;
 	ctx.beginPath();
 	ctx.moveTo(1000-313,111);
 	ctx.bezierCurveTo(1000-296,121,1000-313,150,1000-321,197);
@@ -97,7 +97,7 @@ const drawController = function() {
 	ctx.bezierCurveTo(1000-458,297,1000-486,297,1000-500,297);
 	ctx.stroke();
 	//TOUCHPAD LEFT
-	ctx.strokeStyle = "#FF0000";
+	ctx.strokeStyle = ControllerDrawSettings.outline_color;
 	ctx.beginPath();
 	ctx.moveTo(500,84)
 	ctx.bezierCurveTo(420,83,359,87,335,91);
@@ -106,7 +106,7 @@ const drawController = function() {
 	ctx.bezierCurveTo(437,285,496,285,500,285);
 	ctx.stroke()
 	//TOUCHPAD RIGHT
-	ctx.strokeStyle = "#FF0000";
+	ctx.strokeStyle = ControllerDrawSettings.outline_color;
 	ctx.beginPath();
 	ctx.moveTo(1000-500,84)
 	ctx.bezierCurveTo(1000-420,83,1000-359,87,1000-335,91);
@@ -115,19 +115,19 @@ const drawController = function() {
 	ctx.bezierCurveTo(1000-437,285,1000-496,285,1000-500,285);
 	ctx.stroke()
 	//LITTLE NOTCH ABOVE LED LEFT
-	ctx.strokeStyle = "#FF0000";
+	ctx.strokeStyle = ControllerDrawSettings.outline_color;
 	ctx.beginPath();
 	ctx.moveTo(291,102);
 	ctx.bezierCurveTo(303,97,316,95,321,96);
 	ctx.stroke();
 	//LITTLE NOTCH ABOVE LED RIGHT
-	ctx.strokeStyle = "#FF0000";
+	ctx.strokeStyle = ControllerDrawSettings.outline_color;
 	ctx.beginPath();
 	ctx.moveTo(1000-291,102);
 	ctx.bezierCurveTo(1000-303,97,1000-316,95,1000-321,96);
 	ctx.stroke();
 	//DPAD UP
-	ctx.strokeStyle = "#FF0000";
+	ctx.strokeStyle = ControllerDrawSettings.outline_color;
 	ctx.beginPath();
 	ctx.moveTo(185,221);
 	ctx.bezierCurveTo(171,168,196,178,206,175);
@@ -135,7 +135,7 @@ const drawController = function() {
 	ctx.bezierCurveTo(205,250,210,250,185,221);
 	ctx.stroke();
 	//DPAD RIGHT
-	ctx.strokeStyle = "#FF0000";
+	ctx.strokeStyle = ControllerDrawSettings.outline_color;
 	ctx.beginPath();
 	ctx.moveTo(241,229);
 	ctx.bezierCurveTo(262,228,288,214,287,253);
@@ -143,7 +143,7 @@ const drawController = function() {
 	ctx.bezierCurveTo(214,249,212,260,241,229);
 	ctx.stroke();
 	//DPAD DOWN
-	ctx.strokeStyle = "#FF0000";
+	ctx.strokeStyle = ControllerDrawSettings.outline_color;
 	ctx.beginPath();
 	ctx.moveTo(185,285);
 	ctx.bezierCurveTo(180,320,176,332,209,331);
@@ -151,7 +151,7 @@ const drawController = function() {
 	ctx.bezierCurveTo(201,258,216,257,185,285);
 	ctx.stroke();
 	//DPAD LEFT
-	ctx.strokeStyle = "#FF0000";
+	ctx.strokeStyle = ControllerDrawSettings.outline_color;
 	ctx.beginPath();
 	ctx.moveTo(174,277);
 	ctx.bezierCurveTo(141,282,130,285,130,251);
@@ -159,7 +159,7 @@ const drawController = function() {
 	ctx.bezierCurveTo(204,255,204,251,174,277);
 	ctx.stroke();
 	//LEFT BUMPER
-	ctx.strokeStyle = "#FF0000";
+	ctx.strokeStyle = ControllerDrawSettings.outline_color;
 	ctx.beginPath();
 	ctx.moveTo(151,119);
 	ctx.bezierCurveTo(152,113,153,106,153,105);
@@ -167,7 +167,7 @@ const drawController = function() {
 	ctx.bezierCurveTo(274,85,275,93,274,96);
 	ctx.stroke();
 	//RIGHT BUMPER
-	ctx.strokeStyle = "#FF0000";
+	ctx.strokeStyle = ControllerDrawSettings.outline_color;
 	ctx.beginPath();
 	ctx.moveTo(1000-151,119);
 	ctx.bezierCurveTo(1000-152,113,1000-153,106,1000-153,105);
@@ -175,7 +175,7 @@ const drawController = function() {
 	ctx.bezierCurveTo(1000-274,85,1000-275,93,1000-274,96);
 	ctx.stroke();
 	//SELECT
-	ctx.strokeStyle = "#FF0000";
+	ctx.strokeStyle = ControllerDrawSettings.outline_color;
 	ctx.beginPath();
 	ctx.moveTo(271,154);
 	ctx.bezierCurveTo(265,137,270,129,278,129);
@@ -184,7 +184,7 @@ const drawController = function() {
 	ctx.bezierCurveTo(277,171,276,170,271,154);
 	ctx.stroke();
 	//START
-	ctx.strokeStyle = "#FF0000";
+	ctx.strokeStyle = ControllerDrawSettings.outline_color;
 	ctx.beginPath();
 	ctx.moveTo(1000-271,154);
 	ctx.bezierCurveTo(1000-265,137,1000-270,129,1000-278,129);
@@ -193,37 +193,37 @@ const drawController = function() {
 	ctx.bezierCurveTo(1000-277,171,1000-276,170,1000-271,154);
 	ctx.stroke();
 	//LSTICK
-	ctx.strokeStyle = "#FF0000";
+	ctx.strokeStyle = ControllerDrawSettings.outline_color;
 	ctx.beginPath();
 	ctx.arc(349,381,55,0,2*Math.PI);
 	ctx.stroke();
 	//RSTICK
-	ctx.strokeStyle = "#FF0000";
+	ctx.strokeStyle = ControllerDrawSettings.outline_color;
 	ctx.beginPath();
 	ctx.arc(1000-349,381,55,0,2*Math.PI);
 	ctx.stroke();
 	//TRIANGLE
-	ctx.strokeStyle = "#FF0000";
+	ctx.strokeStyle = ControllerDrawSettings.outline_color;
 	ctx.beginPath();
 	ctx.arc(792,184,30,0,2*Math.PI);
 	ctx.stroke();
 	//CIRCLE
-	ctx.strokeStyle = "#FF0000";
+	ctx.strokeStyle = ControllerDrawSettings.outline_color;
 	ctx.beginPath();
 	ctx.arc(860,253,30,0,2*Math.PI);
 	ctx.stroke();
 	//X
-	ctx.strokeStyle = "#FF0000";
+	ctx.strokeStyle = ControllerDrawSettings.outline_color;
 	ctx.beginPath();
 	ctx.arc(792,322,30,0,2*Math.PI);
 	ctx.stroke();
 	//SQUARE
-	ctx.strokeStyle = "#FF0000";
+	ctx.strokeStyle = ControllerDrawSettings.outline_color;
 	ctx.beginPath();
 	ctx.arc(723,253,30,0,2*Math.PI);
 	ctx.stroke();
 	//SCREENSHOT
-	ctx.strokeStyle = "#FF0000";
+	ctx.strokeStyle = ControllerDrawSettings.outline_color;
 	ctx.beginPath();
 	ctx.arc(483,431,6,Math.PI/2,3*Math.PI/2);
 	ctx.lineTo(517,425);
@@ -231,7 +231,7 @@ const drawController = function() {
 	ctx.lineTo(483,437);
 	ctx.stroke();
 	//PS BUTTON
-	ctx.strokeStyle = "#FF0000";
+	ctx.strokeStyle = ControllerDrawSettings.outline_color;
 	ctx.beginPath();
 	ctx.moveTo(516,392);
 	ctx.bezierCurveTo(542,385,527,380,518,377);
