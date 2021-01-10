5b31d86ce027e4a82e4514619788d22ca5938ca6
Matthew Kelly <quizzlesticks@gmail.com>
2021-01-10 04:40:25 -0700

    I made a whole sprite manager to make things easy. Added a README.

--- PixelTwinShooter/js/main.js
+++ PixelTwinShooter/js/main.js
@@ -4,4 +4,30 @@ const ctx = canvas.getContext("2d");
 canvas.width = 1000;
 canvas.height = 750;
 
+const sprite_folder = "./Spritesheets/";
+
+ctx.fillStyle = "red";
 ctx.fillRect(0,0,canvas.width,canvas.height);
+
+var ssm = new SpriteSheetManager(ctx);
+ssm.load(sprite_folder + "Sprite-0001.png", "player", 44, 44, 1, 2);
+ssm.defineAnimationLoop("player", 0, 1);
+ssm.load(sprite_folder + "green.png", "green", 64,72,4,3);
+ssm.defineAnimationLoop("green", 0, 11);
+ssm.whenFinishedLoading = initGame;
+
+function initGame() {
+    ctx.fillStyle = "black";
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+    setInterval(drawBoiLoop, 250);
+}
+
+function drawBoiLoop() {
+    window.requestAnimationFrame(animate);
+}
+
+function animate() {
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+    ssm.drawNext("player",10,10);
+    ssm.drawNext("green",100,100);
+}
