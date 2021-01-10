2729420d9b2a36158d4a617b51d2290ea78ad068
Matthew Kelly <quizzlesticks@gmail.com>
2021-01-10 01:32:52 -0700

    Added initial files

---
+++ PixelTwinShooter/js/main.js
@@ -0,0 +1,7 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext("2d");
+
+canvas.width = 1000;
+canvas.height = 750;
+
+ctx.fillRect(0,0,canvas.width,canvas.height);
