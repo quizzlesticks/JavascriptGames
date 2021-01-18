5c22d2ee0d792b07d3e84378b7c94c564182df73
Matthew Kelly <quizzlesticks@gmail.com>
2021-01-17 19:07:09 -0700

    Map gen makes ten random maps and premakes images for them. Added gitignore rule to make sure random maps aren't pushed but added gitkeep to keep the Map folder in Spritesheets where we would store them when they are made

--- PixelTwinShooter/PythonScripts/ConwayMapGen/gen.py
+++ PixelTwinShooter/PythonScripts/ConwayMapGen/gen.py
@@ -1,25 +1,64 @@
+from PIL import Image
 import random
-grid = []
-step = []
-#grid = [[0,1,0],[1,0,1],[0,0,0]]
-#step = [[0,0,0],[0,0,0],[0,0,0]]
-length = 10
-height = 10
-min_neighbor = 2
-max_neighbor = 4
-grownth_neighbor = 3
-generation = 1
 
-def GenBoards(step, grid):
-    for j in range(height):
-        step_row = []
+def randomBoard(length, height):
+    board = []
+    for i in range(height):
         random_row = []
-        for i in range(length):
-            y = (random.randint(0, 100))%2
-            random_row.append(y)
-            step_row.append(0)
-        grid.append(random_row)
-        step.append(step_row)
+        for j in range(length):
+            random_row.append(int(random.random() >= MAX_RAND_CHANCE))
+        board.append(random_row)
+    return board
+
+def blankBoard(length, height):
+    board = []
+    for i in range(height):
+        row = [0]*length
+        board.append(row)
+    return board
+
+def CheckCells(past, next):
+    for i in range(height):
+        for j in range(length):
+            z = NeighborSum(past, i, j)
+            if past[i][j] == 0:
+                if z > grownth_neighbor:
+                    next[i][j] = 1
+            else:
+                if z < min_neighbor or z >= max_neighbor:
+                    next[i][j] = 0
+                else:
+                    next[i][j] = 1
+
+def NeighborSum(grid, i, j):
+    #Top Left
+    if (i == 0) and (j == 0):
+        z = grid[i+1][j] + grid[i][j+1] + grid[i+1][j+1]
+    #Bottom Left
+    elif (i == (height-1)) and (j == 0):
+        z = grid[i-1][j] + grid[i-1][j+1] + grid[i][j+1]
+    #Top Right
+    elif (i == 0) and (j == (length-1)):
+        z = grid[i][j-1] + grid[i+1][j-1] + grid[i+1][j]
+    #Bottom Right
+    elif (i == (height-1)) and (j == (length-1)):
+        z = grid[i-1][j] + grid[i-1][j-1] + grid[i][j-1]
+    #Left Wall but not corners
+    elif j == 0:
+        z = grid[i-1][j] + grid[i-1][j+1] + grid[i][j+1] + grid[i+1][j+1] + grid[i+1][j]
+    #Top Wall but not corners
+    elif i == 0:
+        z = grid[i][j-1] + grid[i+1][j-1] + grid[i+1][j] + grid[i+1][j+1] + grid[i][j+1]
+    #Right Wall but not corners
+    elif j == (length - 1):
+        z = grid[i-1][j] + grid[i-1][j-1] + grid[i][j-1] + grid[i+1][j-1] + grid[i+1][j]
+    #Bottom Wall but not corners
+    elif i == (height-1):
+        z = grid[i][j+1] + grid[i-1][j+1] + grid[i-1][j] + grid[i-1][j-1] + grid[i][j-1]
+    #All other
+    else:
+        z = grid[i-1][j-1] + grid[i-1][j] + grid[i-1][j+1] + grid[i][j-1] + grid[i][j+1] + grid[i+1][j-1] + grid[i+1][j] + grid[i+1][j+1]
+    return z
 
 def writeGrid(grid, filename="grid.out"):
     f = open(filename, "w")
@@ -27,45 +66,37 @@ def writeGrid(grid, filename="grid.out"):
         f.write(str(row)[1:][:-1])
         f.write("\n")
 
-def NeighborSum(grid, j, i):
-    if (j == 0) and (i == 0):
-        z = grid[i+1][j] + grid[i][j+1] + grid[i+1][j+1]
-    elif (j == 0) and (i == (length-1)):
-        z = grid[i-1][j] + grid[i-1][j+1] + grid[i][j+1]
-    elif j == 0:
-        z = grid[i-1][j] + grid[i-1][j+1] + grid[i][j+1] + grid[i+1][j+1] + grid[i+1][j]
-    elif (i == 0) and (j == (height-1)):
-        z = grid[i][j-1] + grid[i+1][j-1] + grid[i+1][j]
-    elif i == 0:
-        z = grid[i][j-1] + grid[i+1][j-1] + grid[i+1][j] + grid[i+1][j+1] + grid[i][j+1]
-    elif (j == (height-1)) and (i == (length-1)):
-        z = grid[i-1][j] + grid[i-1][j-1] + grid[i][j-1]
-    elif j == (height - 1):
-        z = grid[i-1][j] + grid[i-1][j-1] + grid[i][j-1] + grid[i+1][j-1] + grid[i+1][j]
-    elif i == (length-1):
-        z = grid[i][j+1] + grid[i-1][j+1] + grid[i-1][j] + grid[i-1][j-1] + grid[i][j-1]
-    else:
-        z = grid[i-1][j-1] + grid[i-1][j] + grid[i-1][j+1] + grid[i][j-1] + grid[i][j+1] + grid[i+1][j-1] + grid[i+1][j] + grid[i+1][j+1]
-    return z
 
-def CheckCells(past, next):
-    for j in range(height):
-        for i in range(length):
-            z = NeighborSum(past, j, i)
-            if past[i][j] == 0:
-                if z == grownth_neighbor:
-                    next[i][j] = 1
-            elif past[i][j] == 1:
-                if z >= min_neighbor and z < max_neighbor:
-                    next[i][j] = 1
-                else:
-                    next[i][j] = 0
+def genImageCache(grid, filenumber):
+    im = Image.new("RGB", (32*length, 32*height))
+    tiles = Image.open("../../Spritesheets/tile.png")
+    box1 = (0,0,32,32)
+    box2 = (32,0,64,32)
+    tile1 = tiles.crop(box1) #grass
+    tile2 = tiles.crop(box2) #dirt
+    for i in range(height):
+        for j in range(length):
+            box = (32*j, 32*i, 32*(j+1), 32*(i+1))
+            if(grid[i][j]):
+                im.paste(tile1, box)
+            else:
+                im.paste(tile2, box)
+    im.save("../../Spritesheets/Map/Map" + str(filenumber) + ".png")
 
 #main
-GenBoards(step, grid)
-for tmp in range(generation):
-    CheckCells(grid, step)
-    CheckCells(step, grid)
-
-writeGrid(grid)
-writeGrid(step, "gen.out")
+if __name__ == "__main__":
+    for l in range(9):
+        length = 32
+        height = 26
+        MAX_RAND_CHANCE = 0.9 #Any number greater or equal to this will start alive
+        min_neighbor = 2 #Kill if less than
+        max_neighbor = 10 #Kills if this many or more
+        grownth_neighbor = 1 #Become alive if more than
+        generation = 4
+        cur_grid = randomBoard(length, height)
+        next_grid = blankBoard(length, height)
+        for k in range(generation):
+            CheckCells(cur_grid, next_grid)
+            cur_grid = next_grid
+            next_grid = blankBoard(length, height)
+        genImageCache(cur_grid, l)
